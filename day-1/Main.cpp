20b834c19dc38210ae089fd57a3bda2571c478b1
kundalik_suryawanshi <kundaliksofficial@gmail.com>
2023-01-16 11:47:16 +0530

    basic-concepts

---
+++ day-1/Main.cpp
@@ -0,0 +1,38 @@
+/*
+flow-chart -digramatic representation of an approch
+-terminator-start-end
+-input/output
+-process
+-desicion making 
+-flow
+
+pseudocode - way to represent the logic of problem
+
+programming laguage - laguage wtiten by human and understandale for machine 
+-semantics
+-rules
+-syntax
+
+compiler - compile complete file 
+interpreter - compile code line by line
+IDE - ingrated developement kit
+
+binary language - computer understandale laguage 0 and 1, exeucateble file
+
+*/
+
+// #include <iostream>
+
+
+// int main() {
+//     std::cout << "Hello World!";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" <<endl;
+}
